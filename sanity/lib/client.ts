360914b3b01cd2261b06acc51e1e3792d26c6017
yassinsolo83-eng <yassinsolo83@gmail.com>
2026-09-11 08:53:09 +0300

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@sanity/client'
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId, isSanityConfigured } from '../env'
 
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true,
-})
+export const client = isSanityConfigured
+  ? createClient({ projectId, dataset, apiVersion, useCdn: true })
+  : null
