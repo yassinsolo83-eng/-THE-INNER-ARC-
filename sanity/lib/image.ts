80f2fca318033cd7632b3d13496902d30007858f
yassinsolo83-eng <yassinsolo83@gmail.com>
2026-09-26 12:22:29 +0300

    Delete sanity/lib/image.ts

--- sanity/lib/image.ts
+++
@@ -1,3 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import { client } from './client'
-export function urlFor(source: any) { return client ? createImageUrlBuilder(client).image(source) : null }
